3ce43fda190f66d421142723da50bf57355de648
Vered Shwartz <vered1986@gmail.com>
2019-03-18 00:03:43 +0200

    GloVe scripts

--- source/training/__init__.py
+++ source/training/__init__.py
@@ -1,1 +1,2 @@
+from source.training.compositional import *
 from source.training.distributional import *
--- source/training/distributional/glove/train_all.sh
+++ source/training/distributional/glove/train_all.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-declare -a dims=(100 200 300)
-declare -a windows=(2 5 10)
-
-for window in "${windows[@]}"
-do
-    # Compute co-occurrence matrix once for each window size
-    bash ./compute_cooccurrence.sh ~/git/Embeddings/output/corpus/en_corpus_ngrams_extended ~/git/Embeddings/output/distributional/glove/ $window;
-
-    # Train for each dimension
-    for dim in "${dims[@]}"
-    do
-        echo "nohup bash ./train.sh ~/git/Embeddings/output/corpus/en_corpus_ngrams_extended ~/git/Embeddings/output/distributional/glove/ $window $dim &"
-    done
-    echo ""
-
-    # Convert textual format to binary
-    for dim in "${dims[@]}"
-    do
-        echo "nohup python ../../utils/convert_text_embeddings_to_binary.py ~/git/Embeddings/output/distributional/glove/${dim}d/win${window}/wv.txt &"
-    done
-    echo ""
-done
-
-
-
-#!/bin/bash
-
 # Important notes!
 # 1. Make sure you first download the GloVe implementation from https://github.com/stanfordnlp/GloVe, and have the
 # files under the current directory in a subdirectory called GloVe.
@@ -36,14 +9,16 @@ declare -a dims=(50 100 200 300)
 declare -a windows=(2 5 10)
 
 # Download the GloVe code
-git clone https://github.com/stanfordnlp/GloVe.git glove;
+git clone https://github.com/stanfordnlp/GloVe.git;
+mv GloVe/* .;
+rm GloVe;
 
 # Compute co-occurrence matrix once for each window size
 for window in "${windows[@]}"
 do
     mkdir -p output/distributional/glove/cooc_win${window};
     mkdir -p output/distributional/glove/win${window};
-    bash training/distributional/glove/compute_cooccurrence.sh \
+    bash source/training/distributional/glove/compute_cooccurrence.sh \
             output/en_corpus_bigrams \
             output/distributional/glove/ ${window} &
 done
@@ -55,7 +30,7 @@ do
     for dim in "${dims[@]}"
     do
         mkdir -p output/distributional/glove/${dim}d/win${window}/;
-        bash output/distributional/glove/train.sh \
+        bash source/training/distributional/glove/train.sh \
                 output/en_corpus_bigrams \
                 output/distributional/glove/ ${window} ${dim} &
     done
