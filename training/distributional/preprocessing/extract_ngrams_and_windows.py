ed835164ab8a7b87f6706a93141e48614b4bff86
Vered Shwartz <vered1986@gmail.com>
2019-03-17 15:03:52 +0200

    Count the number of instances in the corpus

--- training/distributional/preprocessing/extract_ngrams_and_windows.py
+++ training/distributional/preprocessing/extract_ngrams_and_windows.py
@@ -6,9 +6,12 @@ ap.add_argument('out_file', help='The output file')
 ap.add_argument('nc_vocab', help='The vocabulary file')
 args = ap.parse_args()
 
+import os
+import tqdm
 import string
 import codecs
 import itertools
+import subprocess
 
 from word_forms.word_forms import get_word_forms
 
@@ -22,6 +25,7 @@ def main():
     with codecs.open(args.nc_vocab, 'r', 'utf-8') as f_in:
         nc_vocab = frozenset([line.strip() for line in f_in])
 
+    logger.info('Computing variations...')
     variations = {}
     for nc in nc_vocab:
         w1, w2 = nc.split('\t')
@@ -29,11 +33,14 @@ def main():
                               for w1_form, w2_form in itertools.product(
                 get_word_forms(w1)['n'], get_word_forms(w2)['n'])])
 
+    logger.info('Counting the number of instances in the corpus')
+    num_instances = corpus_size(args.corpus)
+
     logger.info('Processing...')
     with codecs.open(args.corpus, 'r', 'utf-8') as f_in:
         with codecs.open(args.out_file, 'w', 'utf-8') as f_out:
             try:
-                for line in f_in:
+                for line in tqdm.tqdm(f_in, total=num_instances):
                     for sentence in get_sentences_with_bigrams(line.strip().lower(), variations):
                         f_out.write(sentence + '\n')
 
@@ -58,6 +65,27 @@ def get_sentences_with_bigrams(sentence, variations):
         nc = variations.get(bigram, None)
         if nc is not None:
             yield ' '.join(words[:i] + [nc] + words[i+2:]).strip()
+
+
+def corpus_size(corpus_file):
+    """
+    Count the number of distinct word pairs in the corpus, or read it from the cached file
+    """
+    if os.path.exists(corpus_file + '_num_instances'):
+        with open(corpus_file + '_num_instances') as f_in:
+            num_instances = int(f_in.read().strip())
+
+    else:
+        num_instances = None
+        p = subprocess.Popen(['wc', '-l', corpus_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result, err = p.communicate()
+        if p.returncode == 0:
+            num_instances = int(result.strip().split()[0])
+
+        with open(corpus_file + '_num_instances', 'w') as f_out:
+            f_out.write(str(num_instances) + '\n')
+
+    return num_instances
 
 
 if __name__ == '__main__':
