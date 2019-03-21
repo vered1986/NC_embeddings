2b7246b9668f42a1d8689adfb62506619a635d8c
Vered Shwartz <vered1986@gmail.com>
2019-03-21 11:13:21 +0200

    Add a script for predicting all the compositional vectors for the test set

---
+++ source/training/compositional/predict_composition_models.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+declare -a algorithms=(add full_add matrix)
+declare -a emb_algorithms=(word2vec_sg word2vec_cbow glove fasttext_sg fasttext_cbow)
+declare -a windows=(2 5 10)
+declare -a dims=(100 200 300)
+
+for algorithm in "${algorithms[@]}"
+do
+    for embeddings in "${emb_algorithms[@]}"
+    do
+        for dim in "${dims[@]}"
+        do
+            for window in "${windows[@]}"
+            do
+                python -m source.training.compositional.compute_vectors \
+                            output/compositional${algorithm}/${embeddings}/win${window}/${dim}d/model.tar.gz \
+                            data/ncs_test.txt \
+                            output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/test_vectors &
+            done
+            wait
+        done
+    done
+done
+
+
