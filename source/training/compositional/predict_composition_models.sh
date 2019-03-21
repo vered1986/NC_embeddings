b133d2c42eaf79f3f2ed48a25ac5dc856ce8813e
Vered Shwartz <vered1986@gmail.com>
2019-03-21 11:16:53 +0200

    Missing slash

--- source/training/compositional/predict_composition_models.sh
+++ source/training/compositional/predict_composition_models.sh
@@ -14,7 +14,7 @@ do
             for window in "${windows[@]}"
             do
                 python -m source.training.compositional.compute_vectors \
-                            output/compositional${algorithm}/${embeddings}/win${window}/${dim}d/model.tar.gz \
+                            output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/model.tar.gz \
                             data/ncs_test.txt \
                             output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/test_vectors &
             done
