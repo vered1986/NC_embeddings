4dd4913ac96eca8371e63e224e6b83c37f157687
Vered Shwartz <vered1986@gmail.com>
2019-04-01 12:35:01 +0300

    Paraphrase-based composition

---
+++ source/training/compositional/nc_paraphrases_dataset_reader.py
@@ -0,0 +1,66 @@
+import json
+import logging
+
+from typing import Dict
+from overrides import overrides
+
+from allennlp.data.instance import Instance
+from allennlp.common.file_utils import cached_path
+from allennlp.data.tokenizers import Tokenizer, WordTokenizer
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.fields import LabelField, TextField, IndexField, SpanField
+
+
+logger = logging.getLogger(__name__)
+
+
+@DatasetReader.register("nc_data_reader")
+class NCDatasetReader(DatasetReader):
+    """
+    Reads a jsonl file containing a list of noun compounds, each with paraphrases
+    obtained from back-translation,
+    and creates a dataset for the training of compositional method.
+
+    Expected format for each input line: { 'compound' : w1_w2 , 'paraphrases' : list }
+
+    The output of ``read`` is a list of ``Instance`` s with the fields:
+        nc: ``TextField``
+        paraphrase: ``TextField``
+
+    Parameters
+    ----------
+    token_indexers : ``Dict[str, TokenIndexer]``
+    lazy : ``bool`` (optional, default=False)
+        Passed to ``DatasetReader``.  If this is ``True``, training will start sooner, but will
+        take longer per batch.  This also allows training with datasets that are too large to fit
+        in memory.
+    """
+    def __init__(self,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 lazy: bool = False,
+                 tokenizer: Tokenizer = None) -> None:
+        super().__init__(lazy)
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+        self._tokenizer = tokenizer or WordTokenizer()
+
+    @overrides
+    def _read(self, file_path):
+        with open(cached_path(file_path), "r") as data_file:
+            logger.info("Reading instances from lines in file at: %s", file_path)
+            for line in data_file:
+                example = json.loads(line.strip())
+
+                for paraphrase in example['paraphrases']:
+                    yield self.text_to_instance(example['compound'], paraphrase)
+
+    @overrides
+    def text_to_instance(self, nc: str, paraphrase: str) -> Instance:
+        tokenized_nc = self._tokenizer.tokenize(nc)
+        nc_field = TextField(tokenized_nc, self._token_indexers)
+
+        tokenized_paraphrase = self._tokenizer.tokenize(paraphrase)
+        paraphrase_field = TextField(tokenized_paraphrase, self._token_indexers)
+
+        fields = {'nc': nc_field, 'paraphrase': paraphrase_field}
+        return Instance(fields)
---
+++ source/training/compositional/paraphrase_composition_model.py
@@ -0,0 +1,78 @@
+import torch
+
+from overrides import overrides
+from typing import Dict, Optional
+
+from allennlp.nn import util
+from allennlp.data import Vocabulary
+from allennlp.models.model import Model
+from allennlp.common.checks import check_dimensions_match
+from allennlp.modules import TextFieldEmbedder, Seq2VecEncoder
+
+
+
+@Model.register("paraphrase_composition_model")
+class CompositionModel(Model):
+    """
+    This ``Model`` composes the constituents of a noun compound via LSTM.
+
+    Training is done by minimizing the distance between each noun compound and each of its paraphrases.
+
+    Parameters
+    ----------
+    vocab : ``Vocabulary``, required
+        A Vocabulary, required in order to compute sizes for input/output projections.
+    text_field_embedder : ``TextFieldEmbedder``, required
+        Used to embed the ``tokens`` ``TextField`` we get as input to the model.
+    encoder : ``Seq2VecEncoder``, required
+        The RNN encoder that returns a vector given a list of vectors.
+    """
+    def __init__(self, vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 encoder: Optional[Seq2VecEncoder] = None,) -> None:
+        super(CompositionModel, self).__init__(vocab)
+
+        self.text_field_embedder = text_field_embedder
+        self.loss = torch.nn.MSELoss()
+        self.metrics = {}
+
+        self.encoder = encoder
+        check_dimensions_match(text_field_embedder.get_output_dim(), encoder.get_input_dim(),
+                               "text embedding dim", "encoder input dim")
+
+    @overrides
+    def forward(self,  # type: ignore
+                nc: Dict[str, torch.LongTensor],
+                paraphrase: Dict[str, torch.LongTensor] = None) -> Dict[str, torch.Tensor]:
+        # pylint: disable=arguments-differ
+        """
+        Parameters
+        ----------
+        nc : Dict[str, Variable], required
+            The output of ``TextField.as_array()``.
+        paraphrase : Variable, optional (default = None)
+            The output of ``TextField.as_array()`` for the paraphrase (given during training).
+
+        Returns
+        -------
+        An output dictionary consisting of:
+        loss : torch.FloatTensor, optional
+            A scalar loss to be optimised.
+        """
+        # Embed and encode the noun compound
+        nc_emb = self.text_field_embedder(nc)
+        nc_mask = util.get_text_field_mask(nc_emb)
+        nc_enc = self.encoder(nc_emb, nc_mask).data
+
+        output_dict = {'vector': nc_enc}
+
+        # Embed and encode the paraphrase
+        if paraphrase:
+            paraphrase_emb = self.text_field_embedder(paraphrase)
+            paraphrase_mask = util.get_text_field_mask(paraphrase_emb)
+            paraphrase_enc = self.encoder(paraphrase_emb, paraphrase_mask).data
+
+            # Compute the loss
+            output_dict['loss'] = self.loss(nc_enc, paraphrase_enc)
+
+        return output_dict
