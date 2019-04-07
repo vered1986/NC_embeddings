6fd54cd86822c51046ff11e4990235fc99b8c1e8
Vered Shwartz <vered1986@gmail.com>
2019-04-07 14:42:21 +0300

    ...

--- source/training/paraphrase_based/train_paraphrase_based.sh
+++ source/training/paraphrase_based/train_paraphrase_based.sh
@@ -5,6 +5,21 @@ declare -a emb_algorithms=(word2vec_sg word2vec_cbow glove fasttext_sg fasttext_
 declare -a windows=(2 5 10)
 declare -a dims=(100 200 300)
 
+
+for embeddings in "${emb_algorithms[@]}"
+do
+    for dim in "${dims[@]}"
+    do
+        for window in "${windows[@]}"
+        do
+            # Copy the distributional embeddings and add "dist_" to the beginning of each line
+            gzip -cd output/distributional/${embeddings}/win${window}/${dim}d/embeddings.txt.gz | sed 's/^/dist_/g' > output/paraphrase_based/cooccurrence/${embeddings}/win${window}/${dim}d/embeddings.txt;
+            cp output/paraphrase_based/cooccurrence/${embeddings}/win${window}/${dim}d/embeddings.txt output/paraphrase_based/backtranslation/${embeddings}/win${window}/${dim}d/embeddings.txt;
+        done
+    done
+done
+
+
 for algorithm in "${algorithms[@]}"
 do
     for embeddings in "${emb_algorithms[@]}"
@@ -13,11 +28,28 @@ do
         do
             for window in "${windows[@]}"
             do
-                allennlp train source/training/paraphrase_based/configurations/${algorithm}/${embeddings}_win${window}_${dim}d.json \
-                -s output/paraphrase_based/${algorithm}/${embeddings}/win${window}/${dim}d/ \
-                --include-package source &
+                python -m source.training.compositional.compute_vectors \
+                            output/paraphrase_based/${algorithm}/${embeddings}/win${window}/${dim}d/model.tar.gz \
+                            data/nc_vocab.txt \
+                            output/paraphrase_based/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt &
             done
             wait
         done
     done
 done
+
+for algorithm in "${algorithms[@]}"
+do
+    for embeddings in "${emb_algorithms[@]}"
+    do
+        for dim in "${dims[@]}"
+        do
+            for window in "${windows[@]}"
+            do
+                gunzip output/paraphrase_based/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt &
+            done
+            wait
+        done
+    done
+done
+
