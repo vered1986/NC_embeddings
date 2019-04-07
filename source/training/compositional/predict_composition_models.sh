caa21f661d7695c3d0129ce2b897ce0c241a0308
Vered Shwartz <vered1986@gmail.com>
2019-04-07 14:40:40 +0300

    Change the script

--- source/training/compositional/predict_composition_models.sh
+++ source/training/compositional/predict_composition_models.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-declare -a algorithms=(add full_add matrix lstm)
+declare -a algorithms=(full_add matrix lstm)
 declare -a emb_algorithms=(word2vec_sg word2vec_cbow glove fasttext_sg fasttext_cbow)
 declare -a windows=(2 5 10)
 declare -a dims=(100 200 300)
+
+
+for embeddings in "${emb_algorithms[@]}"
+do
+    for dim in "${dims[@]}"
+    do
+        for window in "${windows[@]}"
+        do
+            # Copy the distributional embeddings and add "dist_" to the beginning of each line
+            gzip -cd output/distributional/${embeddings}/win${window}/${dim}d/embeddings.txt.gz | sed 's/^/dist_/g' > output/compositional/add/${embeddings}/win${window}/${dim}d/embeddings.txt;
+
+            for algorithm in "${algorithms[@]}"
+            do
+                cp output/compositional/add/${embeddings}/win${window}/${dim}d/embeddings.txt output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt;
+            done
+        done
+    done
+done
+
+declare -a algorithms=(add full_add matrix lstm)
 
 for algorithm in "${algorithms[@]}"
 do
@@ -13,23 +33,28 @@ do
         do
             for window in "${windows[@]}"
             do
-                (
-
-                # Copy the distributional embeddings and add "dist_" to the beginning of each line
-                gzip -cd output/distributional/${embeddings}/win${window}/${dim}d/embeddings.txt.gz | sed 's/^/dist_/g' > output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt;
-
-                # Append the compositional embeddings
                 python -m source.training.compositional.compute_vectors \
                             output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/model.tar.gz \
                             data/nc_vocab.txt \
-                            output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt ;
-
-                # Zip
-                gunzip output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt;
-
-                ) &
+                            output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt &
             done
             wait
         done
     done
 done
+
+for algorithm in "${algorithms[@]}"
+do
+    for embeddings in "${emb_algorithms[@]}"
+    do
+        for dim in "${dims[@]}"
+        do
+            for window in "${windows[@]}"
+            do
+                gunzip output/compositional/${algorithm}/${embeddings}/win${window}/${dim}d/embeddings.txt &
+            done
+            wait
+        done
+    done
+done
+
